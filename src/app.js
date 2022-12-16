f8b34be9e9a468f8389d06ce327e38f517610a10
Jasonmugi <58854927+Jasonmugi@users.noreply.github.com>
2022-12-16 06:52:20 +0300

    add styling

--- src/app.js
+++ src/app.js
@@ -1,30 +1,30 @@
-
-const navToggle = document.querySelector( "#nav-toggle" );
+const navToggle = document.querySelector("#nav-toggle");
 const navUl = document.querySelector("nav ul");
 const bgOverlay = document.querySelector(".bgOverlay");
 
-navToggle.addEventListener( "click", function(e) {
-    navToggle.classList.toggle( "active" );
-    navUl.classList.toggle("activate");
-    bgOverlay.classList.toggle("activate");
-    e.stopPropagation();
-});
-
-document.querySelector(".bgOverlay").addEventListener('click', 
-function(e){
+navToggle.addEventListener("click", function (e) {
   navToggle.classList.toggle("active");
   navUl.classList.toggle("activate");
   bgOverlay.classList.toggle("activate");
-})
+  e.stopPropagation();
+});
 
-window.addEventListener('scroll', () => {
+document.querySelector(".bgOverlay").addEventListener("click", function (e) {
+  navToggle.classList.toggle("active");
+  navUl.classList.toggle("activate");
+  bgOverlay.classList.toggle("activate");
+});
 
-if(window.scrollY > window.innerHeight/2 && document.querySelector("#nav-toggle").classList.contains("active")){
-  navToggle.classList.remove("active");
-  navUl.classList.remove("activate");
-  bgOverlay.classList.remove("activate");
-  console.log("crossed half of viewport!!!");
-}
+window.addEventListener("scroll", () => {
+  if (
+    window.scrollY > window.innerHeight / 20 &&
+    document.querySelector("#nav-toggle").classList.contains("active")
+  ) {
+    navToggle.classList.remove("active");
+    navUl.classList.remove("activate");
+    bgOverlay.classList.remove("activate");
+    console.log("crossed half of viewport!!!");
+  }
 });
 
 /*
